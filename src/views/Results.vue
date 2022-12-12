b23543c6831bbd477ca2c42f4f18c4e0004473ba
Elena Lorente <lorentedev@gmail.com>
2022-12-12 22:23:56 +0100

    Add placeholder results view

---
+++ src/views/Results.vue
@@ -0,0 +1,50 @@
+<template>
+  <section
+    v-if="!loading"
+    id="results-container">
+    <h1>Results</h1>
+  </section>
+  <Loading v-else/>
+</template>
+
+<script>
+import axios from 'axios'
+import Loading from '@/components/Loading.vue'
+
+export default {
+  name: 'Results',
+  components: {
+    Loading
+  },
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    search () {
+      axios({
+          method: 'get',
+          url: `https://www.doctori.com/coche/prices`,
+          headers: {
+            Authentication: '68NkpNZfYNOaefCGCTXcWXngT7EEhiQf1hMxaXpE9TYqMqtcttIqbnxW0sQG1mM8ZgzeV0TEyhn'
+          }
+        })
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    }
+  },
+  beforeMount() {
+    this.search()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
