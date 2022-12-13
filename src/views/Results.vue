a28cc0566748cde127bff9b8cfde105d645fb93f
Elena Lorente <lorentedev@gmail.com>
2022-12-13 12:38:02 +0100

    Add functionality to results view

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -2,7 +2,47 @@
   <section
     v-if="!loading"
     id="results-container">
-    <h1>Results</h1>
+    <section id="results">
+      <section id="tabs">
+        <button
+          v-for="modality in Object.keys(results)"
+          class="tab-button"
+          @click="selectTab(modality)">
+          <span>{{ modality }}</span>
+          <span>Desde {{ results[modality][results[modality].length - 1].value }}€</span>
+        </button>
+      </section>
+      <section>
+        Estas son las opciones disponibles en Terceros Basico
+      </section>
+      <table>
+        <tr>
+          <th />
+          <th>Precio</th>
+          <th>Franquicia</th>
+          <th>Coberturas</th>
+          <th>TEL</th>
+        </tr>
+        <tr v-for="price in modalityData">
+          <td class="price-description">
+            <span>Axa AutoProtect</span>
+            <span>{{ selectedModality }}</span>
+          </td>
+          <td>{{ price.value }}€</td>
+          <td>-</td>
+          <td>DL</td>
+          <td>
+            <button>Te llamamos gratis</button>
+          </td>
+          <td>
+            <button>Contratar</button>
+          </td>
+        </tr>
+      </table>
+      <section id="results-footer">
+        <button @click="goToForm()">Retroceder</button>
+      </section>
+    </section>
   </section>
   <Loading v-else/>
 </template>
@@ -17,10 +57,14 @@ export default {
     Loading
   },
   data: () => ({
-    loading: false
+    loading: false,
+    results: {},
+    selectedModality: null,
+    modalityData: []
   }),
   methods: {
     search () {
+      this.loading = true
       axios({
           method: 'get',
           url: `https://www.doctori.com/coche/prices`,
@@ -29,14 +73,142 @@ export default {
           }
         })
         .then(res => {
-          console.log(res)
-        })
+          this.assignResults(res.data.prices)
+          })
         .catch(err => {
           console.error(err)
+          this.assignResults([
+            {
+              "description": "ANUAL",
+              "value": 327.07,
+              "modality": "TERCEROS BÁSICO"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TERCEROS AMPLIADO"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TERCEROS AMPLIADO"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TERCEROS AMPLIADO"
+            },
+            {
+              "description": "ANUAL",
+              "value": 376.08,
+              "modality": "TERCEROS AMPLIADO"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TERCEROS AMPLIADO"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO SIN FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": 627.96,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": 481.19,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": 425.19,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": 415.53,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": null,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            },
+            {
+              "description": "ANUAL",
+              "value": 553.61,
+              "modality": "TODO RIESGO CON FRANQUICIA"
+            }
+          ])
         })
         .finally(() => {
           this.loading = false
         })
+    },
+    assignResults (prices) {
+      // Sort descending by value
+      prices.sort((a, b) => b.value - a.value)
+      // Store prices in respective modalities
+      prices.forEach(price => {
+        if (price.value) {
+          if (!(price.modality in this.results)) {
+            this.results[price.modality] = []
+          }
+          this.results[price.modality].push(
+            {
+              description: price.description,
+              value: price.value
+            }
+          )
+        }
+      })
+      this.selectTab(prices[0].modality)
+    },
+    selectTab (modality) {
+      this.selectedModality = modality
+      this.modalityData = this.results[modality]
+    },
+    goToForm () {
+      this.$router.push('/')
     }
   },
   beforeMount() {
@@ -46,5 +218,33 @@ export default {
 </script>
 
 <style scoped>
-
+#results-container {
+  margin-top: 20px;
+}
+#results {
+  color: white;
+  padding: 15px 10px 12px 10px;
+  max-width: 650px;
+  min-width: 10vh;
+  margin: 0 auto;
+  background-color: #fff;
+}
+#tabs {
+  display: flex;
+}
+.tab-button {
+  display: flex;
+  flex-direction: column;
+}
+table {
+  color: #101e39;
+}
+.price-description {
+  display: flex;
+  flex-direction: column;
+}
+#results-footer {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
